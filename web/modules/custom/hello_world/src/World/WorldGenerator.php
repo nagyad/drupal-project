ff5bcf2288e8de9e54123c8ed180ead0e1aa32e0
nagyad <joevagyok@gmail.com>
2018-02-25 14:08:21 +0100

    Implement example for caching method

--- web/modules/custom/hello_world/src/World/WorldGenerator.php
+++ web/modules/custom/hello_world/src/World/WorldGenerator.php
@@ -2,9 +2,30 @@
 
 namespace Drupal\hello_world\World;
 
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
 class WorldGenerator {
 
-  public function getHelloWorld() {
-    return 'Hello World';
+  private $keyValueFactory;
+
+  public function __construct(KeyValueFactoryInterface $keyValueFactory) {
+    $this->keyValueFactory = $keyValueFactory;
   }
+
+  public function getHelloWorld() {
+
+    $key = 'hello_world';
+    $store = $this->keyValueFactory->get('world');
+
+    if ($store->has($key)) {
+      return $store->get($key);
+    }
+
+    sleep(2);
+    $string = 'Hello World';
+    $store->set($key, $string);
+
+    return $string;
+  }
+
 }
