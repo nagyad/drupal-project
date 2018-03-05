02ba77d44d2cdee0bdd89f8aa6b4551a5122a49c
nagyad <joevagyok@gmail.com>
2018-03-05 20:18:58 +0100

    Create configuration for caching

--- web/modules/custom/hello_world/src/World/WorldGenerator.php
+++ web/modules/custom/hello_world/src/World/WorldGenerator.php
@@ -7,24 +7,28 @@ use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
 class WorldGenerator {
 
   private $keyValueFactory;
+  private $useCache;
 
-  public function __construct(KeyValueFactoryInterface $keyValueFactory) {
+  public function __construct(KeyValueFactoryInterface $keyValueFactory, $useCache) {
     $this->keyValueFactory = $keyValueFactory;
+    $this->useCache = $useCache;
   }
 
   public function getHelloWorld() {
-
     // Cache example.
     $key = 'hello_world';
     $store = $this->keyValueFactory->get('world');
 
-    if ($store->has($key)) {
+    if ($this->useCache && $store->has($key)) {
       return $store->get($key);
     }
 
     sleep(2);
+
     $string = 'Hello World';
-    $store->set($key, $string);
+    if ($this->useCache){
+      $store->set($key, $string);
+    }
 
     return $string;
   }
